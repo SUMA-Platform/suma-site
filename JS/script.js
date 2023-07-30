9e0908d295ad61a36beb4676c242a7c3a2c33e53
Junnu Salovaara <junnusalovaara@gmail.com>
2023-07-30 20:58:24 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -37,22 +37,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 if(document.getElementById("nav-icon-open")) {
-  document.getElementById("nav-icon-open").addEventListener("click", function(){
-      console.log("Open clicked");
-      document.getElementById("nav-links-mobile").style.width = "100%";
-      document.getElementById("nav-icon-open").style.display = "none";
-      document.getElementById("nav-icon-close").style.display = "block";
-      document.body.classList.add("no-scroll");
-  });
+  document.getElementById("nav-icon-open").addEventListener("click", openMenu);
+  document.getElementById("nav-icon-open").addEventListener("touchstart", openMenu);
 }
 
 if(document.getElementById("nav-icon-close")) {
-  document.getElementById("nav-icon-close").addEventListener("click", function(){
-      document.getElementById("nav-links-mobile").style.width = "0%";
-      document.getElementById("nav-icon-open").style.display = "block";
-      document.getElementById("nav-icon-close").style.display = "none";
-      document.body.classList.remove("no-scroll");
-  });
+  document.getElementById("nav-icon-close").addEventListener("click", closeMenu);
+  document.getElementById("nav-icon-close").addEventListener("touchstart", closeMenu);
 }
+
+function openMenu() {
+    console.log("Open clicked");
+    document.getElementById("nav-links-mobile").style.width = "100%";
+    document.getElementById("nav-icon-open").style.display = "none";
+    document.getElementById("nav-icon-close").style.display = "block";
+    document.body.classList.add("no-scroll");
+}
+
+function closeMenu() {
+    document.getElementById("nav-links-mobile").style.width = "0%";
+    document.getElementById("nav-icon-open").style.display = "block";
+    document.getElementById("nav-icon-close").style.display = "none";
+    document.body.classList.remove("no-scroll");
+}
+
 
 
